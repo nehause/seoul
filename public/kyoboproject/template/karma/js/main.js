cf40eeaa5ad4680997884d1e8cdcf0515a7a68e4
nehause <wlsgnlwo@gmail.com>
2022-08-08 10:24:56 +0900

    commit

--- public/kyoboproject/template/karma/js/main.js
+++ public/kyoboproject/template/karma/js/main.js
@@ -578,30 +578,3 @@ $(document).ready(function(){
   
 
  });
-// new function
-
-/*function bookReviewShow() {
-		  var BRdiv = document.getElementById('bookReview');
-		  var Bbtn = document.getElementById('bookBtn');
-		  
-		  if(BRdiv.style.display == 'none')  {
-		    BRdiv.style.display = 'block';
-		    Bbtn.value = '닫기';
-		    
-		  }else {
-		    BRdiv.style.display = 'none';
-		    Bbtn.value = '더보기';
-		  }
-		} 
-		function publisherReviewShow() {
-			var PRdiv = document.getElementById('publiserReview');
-			var Pbtn = document.getElementById('publisherBtn');
-			
-			if(PRdiv.style.display == 'none')  {
-				  PRdiv.style.display = 'block';
-				  Pbtn.value = '닫기';
-			}else {
-				  PRdiv.style.display = 'none';
-				  Pbtn.value = '더보기';
-			}
-		}*/
